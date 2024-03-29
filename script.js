0b137d66b57a25ade45833ba49dd7e25fcf3573d
Islam Elsayed <102868927+islamelsayed0@users.noreply.github.com>
2024-03-29 01:09:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  // Get all the elements with class="accordion-button" inside the container
+var acc = document.getElementsByClassName("accordion-button");
+var i;
+
+// Loop through the buttons and add the active class to the current control button and toggle between hiding and showing the active panel
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
